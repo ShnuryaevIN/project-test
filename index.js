370068f81003b01de46fa33b5e25ccf0d7ad28cb
Shnuryaev I.N <shnuryaevin@gmail.com>
2024-02-11 07:55:53 +0500

    New fail index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict"
+let summ = 0;
+let arr = [1, 3, 4, 5, 6, 7];
+function calc() {
+    for {let elem of arr) {
+        if(elem % 2 == 0) {
+            summ += elem;
+        }
+    }}
+    return summ;
+}
+calc();
