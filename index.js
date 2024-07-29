39c7d1831fded27a8643991f4cf2efba4699c64e
Shnuryaev I.N <shnuryaevin@gmail.com>
2024-07-29 23:46:14 +0500

    Мой первый коммит

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-"use strict"
-let summ = 0;
-let arr = [1, 3, 4, 5, 6, 7];
-function calc() {
-    for {let elem of arr) {
-        if(elem % 2 == 0) {
-            summ += elem;
-        }
+class Employee {
+    constructor(name, salary) {
+        this.name = name;
+        this.salary = salary;
     }
-    return summ;
+
+    showName() {
+        console.log(this.name);
+    }
+
+    showSalary() {
+        console.log(this.salary);
+    }
 }
-calc();
+
+let user = new Employee("Ivan", 2000);
+
+user.showName();
+user.showSalary();
