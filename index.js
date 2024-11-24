3c4a0cb67448e0fbac97ed2362e91fd360dd472f
Shnuryaev I.N <shnuryaevin@gmail.com>
2024-11-24 23:10:54 +0500

    Доработка всех файлов

--- index.js
+++ index.js
@@ -1,19 +1,2 @@
-class Employee {
-    constructor(name, salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    showName() {
-        console.log(this.name);
-    }
-
-    showSalary() {
-        console.log(this.salary);
-    }
-}
-
-let user = new Employee("Ivan", 2000);
-
-user.showName();
-user.showSalary();
+let text = document.querySelector("p");
+console.log(text);
