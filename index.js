8f59ce8c161036257e4c045762bf4a065d705332
Shnuryaev I.N <shnuryaevin@gmail.com>
2024-11-25 20:23:40 +0500

    Внесены изменения

--- index.js
+++ index.js
@@ -2,12 +2,11 @@
 let summ = 0;
 let arr = [1, 3, 4, 5, 6, 7];
 function calc() {
-    for {let elem of arr) {
+    for (let elem of arr) {
         if(elem % 2 == 0) {
             summ += elem;
         }
-        return;
+        return summ;
     }}
-    return summ;
-}
-calc();
+    
+console.log(calc());
